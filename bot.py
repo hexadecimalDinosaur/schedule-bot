2aa6acb1c31a6345a136d69f6db292d2181fe811
UserBlackBox <userblackbox@tutanota.com>
2020-11-05 12:12:17 -0500

    logging

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import discord
+import logging
 import json
 import datetime
 
@@ -246,6 +247,12 @@ async def on_message(message):
                     pass
             await message.channel.send(content[1].upper() + " has been deleted")
             updateFile()
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 token = ""
 with open("token") as f:
