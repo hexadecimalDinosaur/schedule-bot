8339bb56c88d60754553ee1a78eb931320fc80f9
UserBlackBox <userblackbox@tutanota.com>
2020-11-18 18:22:09 -0500

    update help message

--- bot.py
+++ bot.py
@@ -81,7 +81,7 @@ async def on_message(message):
         await message.channel.send(embed=embed)
 
     elif message.content.lower().startswith('$help'):
-        helpMessage = "`$list` - list all joinable courses\n`$join [code]` - add a course to your schedule\n`$leave [code]` - remove a course from your schedule\n`$courses` - view your courses\n`$schedule` - view your schedule for today\n`$schedule YYYY/MM/DD` - view your schedule on a specific day"
+        helpMessage = "`$list` - list all joinable courses\n`$join [code]` - add a course to your schedule\n`$leave [code]` - remove a course from your schedule\n`$courses` - view your courses\n`$schedule` - view your schedule for today\n`$schedule YYYY/MM/DD` - view your schedule on a specific day\n`$getevents [code]` - view assignment board for course\n`addevent [code] [yyyy/mm/dd] [event_name]` - add assignment to board\n`delevent [code] [yyyy/mm/dd] [event_name]` - remove event from board"
         embed=discord.Embed(title="Commands", description=helpMessage, color=0x0160a7)
         await message.channel.send(embed=embed)
 
